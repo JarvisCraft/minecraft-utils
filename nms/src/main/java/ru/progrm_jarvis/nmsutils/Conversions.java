8dad593a625bac69aeabd2039a44fd7dc07fc90e
JarvisCraft <mrjarviscraft@gmail.com>
2018-10-22 21:12:30 +0300

    Cleanup Conversions

--- nms/src/main/java/ru/progrm_jarvis/nmsutils/Conversions.java
+++ nms/src/main/java/ru/progrm_jarvis/nmsutils/Conversions.java
@@ -5,42 +5,45 @@ import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.comphenix.protocol.wrappers.EnumWrappers.Direction;
 import com.comphenix.protocol.wrappers.EnumWrappers.Particle;
 import com.comphenix.protocol.wrappers.Vector3F;
+import lombok.AccessLevel;
 import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 import lombok.experimental.UtilityClass;
 
 /**
  * Utilities for <i>NSM {@literal <}{@literal >} API</i> conversions
  */
 @UtilityClass
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal=true)
 public class Conversions {
 
     // simple objects
-    private final EquivalentConverter<Vector3F> VECTOR_3_F_EQUIVALENT_CONVERTER = Vector3F.getConverter();
+    EquivalentConverter<Vector3F> VECTOR_3_F_EQUIVALENT_CONVERTER = Vector3F.getConverter();
     // enum wrappers
-    private final EquivalentConverter<Direction> DIRECTION_EQUIVALENT_CONVERTER = EnumWrappers.getDirectionConverter();
-    private final EquivalentConverter<Particle> PARTICLE_EQUIVALENT_CONVERTER = EnumWrappers.getParticleConverter();
+    EquivalentConverter<Direction> DIRECTION_EQUIVALENT_CONVERTER = EnumWrappers.getDirectionConverter();
+    EquivalentConverter<Particle> PARTICLE_EQUIVALENT_CONVERTER = EnumWrappers.getParticleConverter();
 
-    public static Object toNms(@NonNull final Vector3F vector3F) {
+    public Object toNms(@NonNull final Vector3F vector3F) {
         return VECTOR_3_F_EQUIVALENT_CONVERTER.getGeneric(vector3F);
     }
 
-    public static Vector3F toVector3F(@NonNull final Object nms) {
+    public Vector3F toVector3F(@NonNull final Object nms) {
         return VECTOR_3_F_EQUIVALENT_CONVERTER.getSpecific(nms);
     }
 
-    public static Object toNms(@NonNull final Direction direction) {
+    public Object toNms(@NonNull final Direction direction) {
         return DIRECTION_EQUIVALENT_CONVERTER.getGeneric(direction);
     }
 
-    public static Direction toDirection(@NonNull final Object nms) {
+    public Direction toDirection(@NonNull final Object nms) {
         return DIRECTION_EQUIVALENT_CONVERTER.getSpecific(nms);
     }
 
-    public static Object toNms(@NonNull final Particle particle) {
+    public Object toNms(@NonNull final Particle particle) {
         return PARTICLE_EQUIVALENT_CONVERTER.getGeneric(particle);
     }
 
-    public static Particle toParticle(@NonNull final Object nms) {
+    public Particle toParticle(@NonNull final Object nms) {
         return PARTICLE_EQUIVALENT_CONVERTER.getSpecific(nms);
     }
 }
