22f4a14b57218c3739c2c0169088bf7fe76e8b4a
JarvisCraft <mrjarviscraft@gmail.com>
2018-11-05 20:54:15 +0300

    Add plugin logic to MapBasedKeyedSchedulerGroup

--- scheduler/src/main/java/ru/progrm_jarvis/minecraft/schedulerutils/misc/SchedulerGroups.java
+++ scheduler/src/main/java/ru/progrm_jarvis/minecraft/schedulerutils/misc/SchedulerGroups.java
@@ -3,10 +3,17 @@ package ru.progrm_jarvis.minecraft.schedulerutils.misc;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.UtilityClass;
+import org.bukkit.Bukkit;
 import org.bukkit.Utility;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -14,21 +21,56 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 @UtilityClass
 public class SchedulerGroups {
 
-    public <K> KeyedSchedulerGroup<K> keyedSchedulerGroup() {
-        return new MapBasedKeyedSchedulerGroup<>(new HashMap<>());
+    public <K> KeyedSchedulerGroup<K> keyedSchedulerGroup(@NonNull final Plugin plugin,
+                                                          final boolean async, final long delay, final long interval) {
+        return new MapBasedKeyedSchedulerGroup<>(plugin, async, delay, interval, new HashMap<>());
     }@Utility
 
-    public <K> KeyedSchedulerGroup<K> concurrentKeyedSchedulerGroup() {
-        return new ConcurrentMapBasedKeyedSchedulerGroup<>(new HashMap<>());
+    public <K> KeyedSchedulerGroup<K> concurrentKeyedSchedulerGroup(@NonNull final Plugin plugin,
+                                                                    final boolean async, final long delay,
+                                                                    final long interval) {
+        return new ConcurrentMapBasedKeyedSchedulerGroup<>(plugin, async, delay, interval, new HashMap<>());
     }
 
     @ToString
     @EqualsAndHashCode(callSuper = true)
-    @RequiredArgsConstructor
     @FieldDefaults(level = AccessLevel.PROTECTED)
     private static class MapBasedKeyedSchedulerGroup<K> extends KeyedSchedulerGroup<K> {
 
+        final AtomicReference<BukkitTask> runnable = new AtomicReference<>();
+
+        @NonNull final Plugin plugin;
+
+        final boolean async;
+        final long delay, interval;
+
         final Map<K, Runnable> tasks;
+
+        public MapBasedKeyedSchedulerGroup(@NonNull final Plugin plugin, final boolean async, final long delay,
+                                           final long interval,
+                                           @NonNull final Map<K, Runnable> tasks) {
+            this.plugin = plugin;
+            this.async = async;
+            this.delay = delay;
+            this.interval = interval;
+            this.tasks = tasks;
+
+            // set up plugin disable hook
+            Bukkit.getPluginManager().registerEvents(new Listener() {
+
+                @EventHandler
+                public void onPluginDisable(final PluginDisableEvent event) {
+                    if (plugin == event.getPlugin()) shutdown();
+                }
+            }, plugin);
+        }
+
+        protected void shutdown() {
+            if (isCancelled()) throw new IllegalStateException(
+                    "Attempt to shutdown an already shut down MapBasedKeyedSchedulerGroup"
+            );
+            cancel();
+        }
 
         @Override
         public void run() {
@@ -36,6 +78,9 @@ public class SchedulerGroups {
         }
 
         public void addTask(K key, @NonNull Runnable task) {
+            runnable.compareAndSet(null, async
+                    ? runTaskTimerAsynchronously(plugin, delay, interval) : runTaskTimer(plugin, delay, interval));
+
             tasks.put(key, task);
         }
 
@@ -57,8 +102,10 @@ public class SchedulerGroups {
         Lock readLock = lock.readLock();
         Lock writeLock = lock.writeLock();
 
-        public ConcurrentMapBasedKeyedSchedulerGroup(@NonNull final Map<K, Runnable> tasks) {
-            super(tasks);
+        public ConcurrentMapBasedKeyedSchedulerGroup(@NonNull final Plugin plugin,
+                                                     final boolean async, final long delay, final long interval,
+                                                     @NonNull final Map<K, Runnable> tasks) {
+            super(plugin, async, delay, interval, tasks);
         }
 
         @Override
