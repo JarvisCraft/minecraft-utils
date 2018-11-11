346178f23813c235c1b0e0d76362e5b4330c4454
JarvisCraft <mrjarviscraft@gmail.com>
2018-11-11 21:01:27 +0300

    Update ObservableFakeEntity method contracts

--- fake-entity/src/main/java/ru/progrm_jarvis/minecraft/fakeentitylib/entity/ObservableFakeEntity.java
+++ fake-entity/src/main/java/ru/progrm_jarvis/minecraft/fakeentitylib/entity/ObservableFakeEntity.java
@@ -1,6 +1,5 @@
 package ru.progrm_jarvis.minecraft.fakeentitylib.entity;
 
-import com.google.common.base.Preconditions;
 import lombok.val;
 import org.bukkit.entity.Player;
 
@@ -17,12 +16,14 @@ public interface ObservableFakeEntity extends FakeEntity {
     /**
      * Gets view distance for this fake entity. This may be not present in which case this returns {@code -1}.
      *
-     * @return positive view distance for this fake entity if present and {@code -1} otherwise
+     * @return non-negative view distance for this fake entity if present and {@code -1} otherwise
      */
     int getViewDistance();
 
     /**
      * Returns {@code true} if the player has this fake entity rendered and {@code false} otherwise.
+     *
+     * @apiNote this method also returns {@code false} if the player specified is not associated with this fake entity
      *
      * @param player player to check
      * @return whether or not the player has this fake entity rendered
@@ -30,7 +31,9 @@ public interface ObservableFakeEntity extends FakeEntity {
     boolean isRendered(Player player);
 
     /**
-     * Returns {@code true} if the player can see this entity and {@code false} otherwise.
+     * Returns {@code true} if the player can theoretically see this entity and {@code false} otherwise.
+     *
+     * @apiNote method does not check whether the player is managed by this entity
      *
      * @param player player to check for ability to see this fake entity
      * @return whether or not the player can see this fake entity
@@ -41,6 +44,8 @@ public interface ObservableFakeEntity extends FakeEntity {
      * Renders the entity for player which will temporarily despawn the entity for him
      * and normally prevent him from receiving any data related to this entity (except the one for rendering it back).
      *
+     * @apiNote method does not check whether the player is managed by this entity
+     *
      * @param player player for whom to unrender the entity
      */
     void render(Player player);
@@ -49,23 +54,25 @@ public interface ObservableFakeEntity extends FakeEntity {
      * Unrenders the entity for player which means temporarily despawning the entity for him and <i>normally</i>
      * preventing him from receiving any data related to this entity (except the one for rendering it back).
      *
+     * @apiNote method does not check whether the player is managed by this entity
+     *
      * @param player player for whom to unrender the entity
      */
     void unrender(Player player);
 
     /**
      * Attempt to rerender this fake entity for player specified.
-     * Rerendering means calling {@link #render(Player)} if the see the entity although he should
+     * Rerendering means calling {@link #render(Player)} if the player does not see the entity although he should
      * or calling {@link #unrender(Player)} if the player sees the entity but should not.
+     *
+     * @implNote this implementation may be ineffective due to both
+     * {@link #containsPlayer(Player)} and {@link #isRendered(Player)} calls
+     * where they can be replaced with single map lookup in common implementations
      *
      * @param player player for whom to attempt to rerender this entity
      */
     default void attemptRerender(final Player player) {
-        Preconditions.checkArgument(
-                containsPlayer(player), "Player attempting to rerender should be associated with this fake entity"
-        );
-
-        if (isRendered(player)) {
+        if (containsPlayer(player)) if (isRendered(player)) {
             if (!canSee(player)) unrender(player); // unrender for player if he sees but shouldn't
         } else if (canSee(player)) render(player); // render for player if doesn't see but should
     }
