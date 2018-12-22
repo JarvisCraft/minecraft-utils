205f03382560440615e16cc6d2e7b882d7a1938b
JarvisCraft <mrjarviscraft@gmail.com>
2018-12-22 23:08:01 +0300

    Add MapUtil.MapFiller

--- commons/src/main/java/ru/progrm_jarvis/minecraft/commons/util/MapUtil.java
+++ commons/src/main/java/ru/progrm_jarvis/minecraft/commons/util/MapUtil.java
@@ -1,12 +1,14 @@
 package ru.progrm_jarvis.minecraft.commons.util;
 
 import com.google.common.base.Preconditions;
-import lombok.NonNull;
+import lombok.*;
+import lombok.experimental.Accessors;
 import lombok.experimental.UtilityClass;
-import lombok.val;
-import lombok.var;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.stream.Stream;
 
 /**
  * Utilities related to {@link Map>}.
@@ -68,5 +70,108 @@ public class MapUtil {
         Object key = null; // requires to be initialized for some reason :)
         for (final Object keyValuePair : keyValuePairs) if (value = !value) map.put(key, keyValuePair);
         else key = keyValuePair;
+    }
+
+    /**
+     * Creates new {@link MapFiller} from the map specified.
+     *
+     * @param map map for which to create the filler
+     * @param <K> type of keys
+     * @param <V> type of values
+     * @return map filler created for the specified map
+     *
+     * @see MapFiller
+     * @see #mapFiller(Map, Object, Object)
+     */
+    public <K, V> MapFiller<K, V> mapFiller(@NonNull final Map<K, V> map) {
+        return new MapFiller<>(map);
+    }
+
+    /**
+     * Creates new {@link MapFiller} from the map specified initialized with the value specified.
+     *
+     * @param map map for which to create the filler
+     * @param firstValueKey the key of first value
+     * @param firstValue first value to be put to the map
+     * @param <K> type of keys
+     * @param <V> type of values
+     * @return map filler created for the specified map with initial value put
+     *
+     * @see MapFiller
+     * @see #mapFiller(Map)
+     */
+    public <K, V> MapFiller<K, V> mapFiller(@NonNull final Map<K, V> map, K firstValueKey, final V firstValue) {
+        return new MapFiller<>(map)
+                .put(firstValueKey, firstValue);
+    }
+
+    /**
+     * An utility-object to fill the map following the chain pattern which may useful when initializing class fields.
+     *
+     * @param <K> type of map's key
+     * @param <V> type of map's value
+     */
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor
+    @Accessors(fluent = true)
+    public static final class MapFiller<K, V> {
+
+        /**
+         * Map being filled
+         */
+        @NonNull @Getter private final Map<K, V> map;
+
+        /**
+         * Puts the specified value by the specified key to the map.
+         *
+         * @param key key of the value to put
+         * @param value value to put by the key
+         * @return this map filler for chaining
+         */
+        public MapFiller<K, V> put(final K key, final V value) {
+            map.put(key, value);
+
+            return this;
+        }
+
+        /**
+         * Fills the map based on the specified {@link Iterator}.
+         *
+         * @param entries iterator of the entries whose elements will be put to the map
+         * @return this map filler for chaining
+         */
+        public MapFiller<K, V> fill(final Iterator<Pair<K, V>> entries) {
+            while (entries.hasNext()) {
+                val entry = entries.next();
+                map.put(entry.getKey(), entry.getValue());
+            }
+
+            return this;
+        }
+
+        /**
+         * Fills the map with the values of specified {@link Iterable}.
+         *
+         * @param entries entries which will be put to the map
+         * @return this map filler for chaining
+         */
+        public MapFiller<K, V> fill(final Iterable<Pair<K, V>> entries) {
+            for (val entry : entries) map.put(entry.getKey(), entry.getValue());
+
+            return this;
+        }
+
+        /**
+         * Fills the map based on the specified {@link Stream}.
+         *
+         * @param entries stream of the entries whose elements will be put to the map
+         * @return this map filler for chaining
+         */
+        public MapFiller<K, V> fill(final Stream<Pair<K, V>> entries) {
+            entries.forEach(entry -> map.put(entry.getKey(), entry.getValue()));
+
+            return this;
+        }
     }
 }
