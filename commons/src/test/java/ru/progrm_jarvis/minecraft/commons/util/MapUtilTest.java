4863ec2d9fa268dd97f77ad12087fe545cda4043
JarvisCraft <mrjarviscraft@gmail.com>
2018-12-23 03:13:08 +0300

    Test MapFiller

--- commons/src/test/java/ru/progrm_jarvis/minecraft/commons/util/MapUtilTest.java
+++ commons/src/test/java/ru/progrm_jarvis/minecraft/commons/util/MapUtilTest.java
@@ -1,5 +1,6 @@
 package ru.progrm_jarvis.minecraft.commons.util;
 
+import lombok.val;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.stream.Stream;
 
+import static com.google.common.collect.Maps.immutableEntry;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapUtilTest {
@@ -92,6 +96,105 @@ class MapUtilTest {
                     put(1, "Hello");
                 }},
                 MapUtil.fillMap(new HashMap<>(), Stream.of(Pair.of(1, "Hello"), Pair.of(2, "world")))
+        );
+    }
+
+    @Test
+    void testMapFillerConstructWithFirst() {
+        assertThat(
+                MapUtil.mapFiller(new HashMap<>()).map().keySet(),
+                empty()
+        );
+
+        assertEquals(
+                new HashMap<String, Integer>() {{
+                    put("Hello", 1);
+                }},
+                MapUtil.mapFiller(new HashMap<>(), "Hello", 1).map()
+        );
+    }
+
+    @Test
+    @SuppressWarnings("unchecked") // Hamcrest, R U fine?
+    void testMapFillerPut() {
+        val entries = MapUtil.mapFiller(new HashMap<String, Integer>())
+                .put("one", 1)
+                .put("two", 2)
+                .map()
+                .entrySet();
+
+        assertThat(entries, hasSize(2));
+
+        assertThat(entries, hasItems(immutableEntry("one", 1), immutableEntry("two", 2)));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked") // Hamcrest, R U fine?
+    void testMapFillerFillIterator() {
+        val entries = MapUtil.mapFiller(new HashMap<String, Integer>())
+                .fill(Arrays.asList(Pair.of("one", 1), Pair.of("two", 2)).iterator())
+                .map()
+                .entrySet();
+
+        assertThat(entries, hasSize(2));
+
+        assertThat(entries, hasItems(immutableEntry("one", 1), immutableEntry("two", 2)));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked") // Hamcrest, R U fine?
+    void testMapFillerFillIterable() {
+        val entries = MapUtil.mapFiller(new HashMap<String, Integer>())
+                .fill(Arrays.asList(Pair.of("one", 1), Pair.of("two", 2)))
+                .map()
+                .entrySet();
+
+        assertThat(entries, hasSize(2));
+
+        assertThat(entries, hasItems(immutableEntry("one", 1), immutableEntry("two", 2)));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked") // Hamcrest, R U fine?
+    void testMapFillerFillStream() {
+        val entries = MapUtil.mapFiller(new HashMap<String, Integer>())
+                .fill(Stream.of(Pair.of("one", 1), Pair.of("two", 2)))
+                .map()
+                .entrySet();
+
+        assertThat(entries,
+                hasSize(2)
+        );
+
+        assertThat(entries, hasItems(immutableEntry("one", 1), immutableEntry("two", 2)));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked") // Hamcrest, R U fine?
+    void testMapFillerFillEveryKind() {
+        val entries = MapUtil.mapFiller(new HashMap<String, Integer>())
+                .put("one", 1)
+                .put("two", 2)
+                .fill(Arrays.asList(Pair.of("five", 5), Pair.of("six", 6)))
+                .fill(Arrays.asList(Pair.of("three", 3), Pair.of("four", 4)))
+                .fill(Stream.of(Pair.of("seven", 7), Pair.of("eight", 8)))
+                .map()
+                .entrySet();
+
+        assertThat(entries, hasSize(8));
+
+        assertThat(
+                entries,
+                hasItems(
+                        immutableEntry("one", 1),
+                        immutableEntry("two", 2),
+                        immutableEntry("three", 3),
+                        immutableEntry("four", 4),
+                        immutableEntry("five", 5),
+                        immutableEntry("six", 6),
+                        immutableEntry("seven", 7),
+                        immutableEntry("eight", 8)
+                )
         );
     }
 }
