e8891e9aece2f000d4bc96577f8b295036cf0ab6
JarvisCraft <mrjarviscraft@gmail.com>
2018-12-31 14:23:10 +0300

    Divide RegistersSelfInPlayerRegistry and ShouldBeRegisteredInPlayerRegistry

--- commons/src/main/java/ru/progrm_jarvis/minecraft/commons/player/registry/RegistersSelfInPlayerRegistry.java
+++ commons/src/main/java/ru/progrm_jarvis/minecraft/commons/player/registry/RegistersSelfInPlayerRegistry.java
@@ -3,11 +3,11 @@ package ru.progrm_jarvis.minecraft.commons.player.registry;
 import java.lang.annotation.*;
 
 /**
- * Marker to indicate that the annotated type should be registered in {@link PlayerRegistry} whenever instantiated.
+ * Marker to indicate that the annotated type self-registered in {@link PlayerRegistries} whenever instantiated.
  */
 @Inherited
 @Documented
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
-public @interface ShouldBeRegisteredInPlayerRegistry {
+public @interface RegistersSelfInPlayerRegistry {
 }
