ed870f7b2986b1b165aa6dca40086e778a275ec7
JarvisCraft <mrjarviscraft@gmail.com>
2019-02-01 16:58:02 +0300

    Add Weak Lazy types

--- commons/src/main/java/ru/progrm_jarvis/minecraft/commons/util/function/lazy/Lazies.java
+++ commons/src/main/java/ru/progrm_jarvis/minecraft/commons/util/function/lazy/Lazies.java
@@ -6,6 +6,9 @@ import lombok.experimental.UtilityClass;
 import ru.progrm_jarvis.minecraft.commons.util.function.UncheckedSupplier;
 
 import javax.annotation.Nullable;
+import java.lang.ref.WeakReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Supplier;
 
 @UtilityClass
@@ -17,6 +20,14 @@ public class Lazies {
 
     public <T> Lazy<T> concurrentLazy(@NonNull final UncheckedSupplier<T> valueSupplier) {
         return new ConcurrentLazy<>(valueSupplier);
+    }
+
+    public <T> Lazy<T> weakLazy(@NonNull final UncheckedSupplier<T> valueSupplier) {
+        return new WeakLazy<>(valueSupplier);
+    }
+
+    public <T> Lazy<T> concurrentWeakLazy(@NonNull final UncheckedSupplier<T> valueSupplier) {
+        return new ConcurrentWeakLazy<>(valueSupplier);
     }
 
     @ToString
@@ -82,6 +93,78 @@ public class Lazies {
             }
 
             return value;
+        }
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    private static final class WeakLazy<@NonNull T> implements Lazy<T> {
+
+        @NonNull final transient Supplier<T> valueSupplier;
+        @Nullable transient WeakReference<T> value;
+
+        @Override
+        public boolean isInitialized() {
+            return value != null && value.get() != null;
+        }
+
+        @Override
+        public T get() {
+            T value;
+            if (this.value == null || (value = this.value.get()) == null) this.value
+                    = new WeakReference<>(value = valueSupplier.get());
+
+            return value;
+        }
+    }
+
+    @ToString
+    @EqualsAndHashCode
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    private static final class ConcurrentWeakLazy<@NonNull T> implements Lazy<T> {
+
+        @NonNull final transient Supplier<T> valueSupplier;
+        @Nullable transient WeakReference<T> value;
+        @NonNull transient final Lock readLock, writeLock;
+
+        private ConcurrentWeakLazy(@NonNull final Supplier<T> valueSupplier) {
+            this.valueSupplier = valueSupplier;
+
+            val lock = new ReentrantReadWriteLock();
+            readLock = lock.readLock();
+            writeLock = lock.writeLock();
+        }
+
+        @Override
+        public boolean isInitialized() {
+            readLock.lock();
+            try {
+                return value != null && value.get() != null;
+            } finally {
+                readLock.unlock();
+            }
+        }
+
+        @Override
+        public T get() {
+            readLock.lock();
+            try {
+                T value;
+                if (this.value == null || (value = this.value.get()) == null) {
+                    writeLock.lock();
+                    try {
+                        this.value = new WeakReference<>(value = valueSupplier.get());
+                    } finally {
+                        writeLock.unlock();
+                    }
+                }
+
+                return value;
+            } finally {
+                readLock.unlock();
+            }
         }
     }
 }
