09897729eb06b8b2e8b3f6642e5a547c0ca7190e
JarvisCraft <mrjarviscraft@gmail.com>
2019-02-28 21:15:06 +0300

    Add PlayerUtil#getOnlinePlayers()

--- commons/src/main/java/ru/progrm_jarvis/minecraft/commons/player/PlayerUtil.java
+++ commons/src/main/java/ru/progrm_jarvis/minecraft/commons/player/PlayerUtil.java
@@ -12,6 +12,16 @@ import java.util.stream.Collectors;
 @UtilityClass
 public class PlayerUtil {
 
+    /**
+     * Gets online players as a collection of exact {@link Player} type.
+     *
+     * @return a view of currently online players cast to exact type
+     */
+    @SuppressWarnings("unchecked")
+    public Collection<Player> getOnlinePlayers() {
+        return (Collection<Player>) Bukkit.getOnlinePlayers();
+    }
+
     public Collection<Player> playersAround(@NonNull final Location location, final double radius) {
         return Bukkit.getOnlinePlayers().stream()
                 .filter(player -> player.getLocation().distance(location) <= radius)
