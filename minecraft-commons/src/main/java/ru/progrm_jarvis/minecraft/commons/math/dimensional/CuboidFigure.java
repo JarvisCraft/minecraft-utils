924cdefec2ae388f2da0e364e2c37a8778e76513
JarvisCraft <mrjarviscraft@gmail.com>
2019-03-26 23:47:08 +0300

    Add Figure3D API

---
+++ minecraft-commons/src/main/java/ru/progrm_jarvis/minecraft/commons/math/dimensional/CuboidFigure.java
@@ -0,0 +1,67 @@
+package ru.progrm_jarvis.minecraft.commons.math.dimensional;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Value;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+/**
+ * A cuboid figure.
+ */
+@Value
+@FieldDefaults(level = AccessLevel.PROTECTED)
+@NonFinal public class CuboidFigure implements Figure3D {
+
+    @Getter double minX, minY, minZ, maxX, maxY, maxZ;
+
+    @Override
+    public boolean contains(final double x, final double y, final double z) {
+        return minX <= x && x <= maxX
+                && minY <= y && y <= maxY
+                && minZ <= z && z <= maxZ;
+    }
+
+    /**
+     * Creates a cubic figure based on two points.
+     *
+     * @param x1 X-coordinate of the first point of the cubic figure
+     * @param y1 T-coordinate of the first point of the cubic figure
+     * @param z1 Z-coordinate of the first point of the cubic figure
+     * @param x2 X-coordinate of the second point of the cubic figure
+     * @param y2 T-coordinate of the second point of the cubic figure
+     * @param z2 Z-coordinate of the second point of the cubic figure
+     * @return created cubic figure including the area between the two points
+     */
+    @NonNull
+    private static Figure3D between(final double x1, final double y1, final double z1,
+                                    final double x2, final double y2, final double z2) {
+        if (x1 == x2 && y1 == y2 && z1 == z2) return new PointFigure(x1, y1, z1);
+        return new CuboidFigure(x1, y1, z1, x2, y2, z2);
+    }
+
+    /**
+     * Creates a cubic figure based on two points.
+     *
+     * @param point1 the first point of the cubic figure
+     * @param point2 the second point of the cubic figure
+     * @return created cubic figure including the area between the two points
+     */
+    public static Figure3D between(@NonNull final Vector point1, @NonNull final Vector point2) {
+        return between(point1.getX(), point1.getY(), point1.getZ(), point2.getX(), point2.getY(), point2.getZ());
+    }
+
+    /**
+     * Creates a cubic figure based on two points.
+     *
+     * @param point1 the first point of the cubic figure
+     * @param point2 the second point of the cubic figure
+     * @return created cubic figure including the area between the two points
+     */
+    public static Figure3D between(@NonNull final Location point1, @NonNull final Location point2) {
+        return between(point1.getX(), point1.getY(), point1.getZ(), point2.getX(), point2.getY(), point2.getZ());
+    }
+}
---
+++ minecraft-commons/src/main/java/ru/progrm_jarvis/minecraft/commons/math/dimensional/Figure3D.java
@@ -0,0 +1,136 @@
+package ru.progrm_jarvis.minecraft.commons.math.dimensional;
+
+import lombok.NonNull;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+/**
+ * A figure existing in 3-dimensional system.
+ */
+public interface Figure3D {
+
+    Figure3D EMPTY = new Figure3D() {
+        //<editor-fold desc="Empty figure singleton class" defaultstate="collapsed">
+
+        @Override
+        public double getMinX() {
+            return Double.NaN;
+        }
+
+        @Override
+        public double getMaxX() {
+            return Double.NaN;
+        }
+
+        @Override
+        public double getMinY() {
+            return Double.NaN;
+        }
+
+        @Override
+        public double getMaxY() {
+            return Double.NaN;
+        }
+
+        @Override
+        public double getMinZ() {
+            return Double.NaN;
+        }
+
+        @Override
+        public double getMaxZ() {
+            return Double.NaN;
+        }
+
+        @Override
+        public boolean contains(final double x, final double y, final double z) {
+            return false;
+        }
+
+        @Override
+        public boolean contains(final @NonNull Vector point) {
+            return false;
+        }
+
+        @Override
+        public boolean contains(final @NonNull Location point) {
+            return false;
+        }
+        //</editor-fold>
+    };
+
+    /**
+     * Gets the minimal figure's coordinate by X axis.
+     *
+     * @return the least X-coordinate of the figure
+     */
+    double getMinX();
+
+    /**
+     * Gets the maximal figure's coordinate by X axis.
+     *
+     * @return the most X-coordinate of the figure
+     */
+    double getMaxX();
+
+    /**
+     * Gets the minimal figure's coordinate by Y axis.
+     *
+     * @return the least Y-coordinate of the figure
+     */
+    double getMinY();
+
+    /**
+     * Gets the maximal figure's coordinate by Y axis.
+     *
+     * @return the most Y-coordinate of the figure
+     */
+    double getMaxY();
+
+    /**
+     * Gets the minimal figure's coordinate by Z axis.
+     *
+     * @return the least Z-coordinate of the figure
+     */
+    double getMinZ();
+
+    /**
+     * Gets the maximal figure's coordinate by Z axis.
+     *
+     * @return the most Z-coordinate of the figure
+     */
+    double getMaxZ();
+
+    /**
+     * Checks whether or not this figure contains the point.
+     *
+     * @param x X coordinate of a point to check
+     * @param y Y coordinate of a point to check
+     * @param z Z coordinate of a point to check
+     *
+     * @return {@code true} if the point belongs to this figure and {@link false} otherwise
+     */
+    boolean contains(double x, double y, double z);
+
+    /**
+     * Checks whether or not this figure contains the point.
+     *
+     * @param point point to check
+     *
+     * @return {@code true} if the point belongs to this figure and {@link false} otherwise
+     */
+    default boolean contains(@NonNull final Vector point) {
+        return contains(point.getX(), point.getY(), point.getZ());
+    }
+
+    /**
+     * Checks whether or not this figure contains the point.
+     *
+     * @param point point to check
+     *
+     * @return {@code true} if the point belongs to this figure and {@link false} otherwise
+     */
+    default boolean contains(@NonNull final Location point) {
+        return contains(point.getX(), point.getY(), point.getZ());
+    }
+}
