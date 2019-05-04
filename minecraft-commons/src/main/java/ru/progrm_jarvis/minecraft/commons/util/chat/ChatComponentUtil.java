20b2c7531d818e224d1411d30ddbe877cac17a42
JarvisCraft <mrjarviscraft@gmail.com>
2019-05-05 02:32:14 +0300

    Add ChatComponentUtil

---
+++ minecraft-commons/src/main/java/ru/progrm_jarvis/minecraft/commons/util/chat/ChatComponentUtil.java
@@ -0,0 +1,48 @@
+package ru.progrm_jarvis.minecraft.commons.util.chat;
+
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+import com.google.gson.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
+
+import java.lang.reflect.Type;
+
+/**
+ * Utility for functions related to chat-components.
+ */
+@UtilityClass
+public class ChatComponentUtil {
+
+    /**
+     * Gets an instance of {@link WrappedChatComponentGsonSerializer}
+     * capable of serializing and deserializing {@link WrappedChatComponent}
+     * which may be used in {@link GsonBuilder#registerTypeAdapter(Type, Object)}.
+     *
+     * @return instance of {@link WrappedChatComponentGsonSerializer} for {@link WrappedChatComponent} (de)serialization
+     */
+    public static WrappedChatComponentGsonSerializer wrappedChatComponentGsonSerializer() {
+        return WrappedChatComponentGsonSerializer.INSTANCE;
+    }
+
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class WrappedChatComponentGsonSerializer
+            implements JsonSerializer<WrappedChatComponent>, JsonDeserializer<WrappedChatComponent> {
+
+        private static final WrappedChatComponentGsonSerializer INSTANCE = new WrappedChatComponentGsonSerializer();
+
+        @Override
+        public WrappedChatComponent deserialize(final JsonElement json, final Type typeOfT,
+                                                final JsonDeserializationContext context)
+                throws JsonParseException {
+            if (json.isJsonObject()) return WrappedChatComponent.fromJson(json.toString());
+            return WrappedChatComponent.fromText(json.getAsString());
+        }
+
+        @Override
+        public JsonElement serialize(final WrappedChatComponent src, final Type typeOfSrc,
+                                     final JsonSerializationContext context) {
+            return context.serialize(context.serialize(src.getJson(), JsonObject.class));
+        }
+    }
+}
