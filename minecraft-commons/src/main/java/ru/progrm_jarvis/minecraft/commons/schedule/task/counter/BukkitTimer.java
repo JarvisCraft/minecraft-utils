a92a4882ea0fcb7e5be9147b0084f9fd4fb7c029
progrm_jarvis <gh@progrm-jarvis.ru>
2020-04-16 23:12:57 +0300

    Use `SchedulerRunnable` in timers

--- minecraft-commons/src/main/java/ru/progrm_jarvis/minecraft/commons/schedule/task/counter/BukkitTimer.java
+++ minecraft-commons/src/main/java/ru/progrm_jarvis/minecraft/commons/schedule/task/counter/BukkitTimer.java
@@ -1,18 +1,24 @@
 package ru.progrm_jarvis.minecraft.commons.schedule.task.counter;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import org.bukkit.scheduler.BukkitRunnable;
+import ru.progrm_jarvis.minecraft.commons.schedule.task.AbstractSchedulerRunnable;
+import ru.progrm_jarvis.minecraft.commons.schedule.task.SchedulerRunnable;
+
+import java.util.function.LongConsumer;
 
 /**
- * A {@link BukkitRunnable} which is run for a specified amount of times after which it is cancelled.
+ * A {@link ru.progrm_jarvis.minecraft.commons.schedule.task.SchedulerRunnable}
+ * which is run for a specified amount of times after which it is cancelled.
  */
-@RequiredArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @FieldDefaults(level = AccessLevel.PROTECTED, makeFinal = true)
-public class BukkitTimer extends BukkitRunnable {
+public class BukkitTimer extends AbstractSchedulerRunnable {
 
     @NonNull Runnable task;
     @NonFinal long counter;
@@ -21,5 +27,9 @@ public class BukkitTimer extends BukkitRunnable {
     public void run() {
         if (--counter == 0) cancel();
         task.run();
+    }
+
+    public static SchedulerRunnable create(@NonNull final Runnable task, final long counter) {
+        return new BukkitTimer(task, counter);
     }
 }
